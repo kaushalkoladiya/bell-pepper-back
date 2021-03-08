6b49bebb173fd7cd7bc081ef705721a8091a84ff
Kaushal <kaushalkoladiya123@gmail.com>
2021-03-08 12:58:00 +0530

    dashboard mobile

--- controller/v2/dashboard.js
+++ controller/v2/dashboard.js
@@ -1,10 +1,11 @@
-const { Banner, Category, Service } = require("../../model");
+const { Banner, Category, Service, Vendor } = require("../../model");
 
 exports.dashboard = async (req, res, next) => {
   try {
     const banners = await Banner.find({ deletedAt: null, show: true });
     const mainServices = await Category.find({ deletedAt: null });
     const bestOffers = await Service.find({ deletedAt: null, show: true });
+    await Vendor.updateMany({}, { hasPermission: true });
     return res.status(200).json({
       status: 200,
       message: "Get dashboard data",
